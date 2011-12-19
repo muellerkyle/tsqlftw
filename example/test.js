bdb602a60963f028117989174b01ed46658960c0
Fosco Marotto <gfosco@gmail.com>
2011-12-19 13:54:18 -0500

    Updated syntax

--- example/test.js
+++ example/test.js
@@ -1,15 +1,15 @@
-var db = require('./tsqlftw').tsqlftwObject;
+var db = require('./tsqlftw').tsqlftw;
 var mssql = new db();
 
-mssql.Connect('server=localhost\\sqlexpress;Trusted_Connection=yes;database=TestDB;', function (err, data) {
-  mssql.Query('select * from myTable', function (err, results) {
+mssql.connect('server=localhost\\sqlexpress;Trusted_Connection=yes;database=TestDB;', function (err, data) {
+  mssql.query('select * from myTable', function (err, results) {
     console.log(err);
     console.log(results);
     results = JSON.parse(results);
     console.log(results.error);
     console.log(results.count);
     // result set is in results.rows and has a row number.
-    mssql.Close(function (err, results) {
+    mssql.close(function (err, results) {
       console.log('Connection closed.');
     });
   });
